5ca0f1fa75bd4eae68237fdda3ee493a93e6a124
NoIAmbad <fantasma.irrealsoy@gmail.com>
2026-05-25 15:49:58 -0600

    Simple example of using the PHP's built-in server

---
+++ 00_built-in_server/index.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	// PHP 5.4+ comes with a built-in development server.
+	//The built-in server is only designed to be used for development
+	//and testing purposes. 
+	//DO NOT use production.
+	
+	//Execute the next comman in the terminal to start the server:
+	// php -S localhost:8080
+	//The command will star the server in the current directory.
+	echo "Example of the PHP's built-in server\n";
+	
+	//Using HTML.
+	echo "<h2>My title</h2>";
+	
+	// Open a browser and navigate to http://localhost:8080
+	//The messages should be displayed in the browser.
