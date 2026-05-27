e47366a305fa18dd60bb1c8519def218aacaf02b
NoIAmbad <fantasma.irrealsoy@gmail.com>
2026-05-27 12:52:55 -0600

    Intro to PHP

---
+++ 01_intro_php/index.php
@@ -0,0 +1,14 @@
+<?php # Opening tag
+
+	# Lenguage construct: echo
+	#Output one o more expression.
+	#In this example it display a message.
+	#The message must goes inside "".
+	#Each statement is terminated with ;
+	echo "Hello, I am PHP script";
+	
+# Closing tag
+#If a file ends with PHP code, it is preferable
+#to omit the PHP closing tag at the end of the file.
+#This tag can be omitted.
+?>
